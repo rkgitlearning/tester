c5fa01d4bdc96c43c82c3b76fa3890cd5d29ef30
rama noothan <ramakrishna.java1@gmail.com>
2018-06-07 07:04:16 +0530

    First 2
    first 3
    
    Added all the 3 commits
    
    Added main method
    
    Added first method
    
    Squashed first 2 commits

---
+++ Raama.java
@@ -0,0 +1,13 @@
+
+public class Raama{
+
+	public static void main(String []rk){
+
+		System.out.println("My name is Raama krishna");
+	}
+
+	public static void first(){
+		System.out.println("This is first method");
+	}
+
+}
